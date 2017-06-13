eac54a943943697d29646d97a4d4940af68600b2
silvanic <alexandre.dezier@orange.fr>
2017-06-13 14:06:43 +0200

    création classe Java

---
+++ src/main/java/MonTest.java
@@ -0,0 +1,8 @@
+import Utils.MyStringUtils;
+
+public class MonTest {
+	public static void main(String[] args) {
+		String debut = "Test is null or empty? : ";
+		System.out.println(debut + MyStringUtils.isNullorEmtpy("test"));
+	}
+}
