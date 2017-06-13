51ceec7f89fcdd443f3736f2ef142f19dfa2dee2
silvanic <alexandre.dezier@orange.fr>
2017-06-13 15:24:07 +0200

    amélioration de MonTest.java

--- src/main/java/MonTest.java
+++ src/main/java/MonTest.java
@@ -2,7 +2,7 @@ import Utils.MyStringUtils;
 
 public class MonTest {
 	public static void main(String[] args) {
-		String debut = "Test is null or empty? : ";
-		System.out.println(debut + MyStringUtils.isNullorEmtpy("Mon super test master GitHub"));
+		String debut = "is null or empty? ";
+		System.out.println("\"Mon super test master GitHub\" : " + debut + MyStringUtils.isNullorEmtpy("Mon super test master GitHub"));
 	}
 }
