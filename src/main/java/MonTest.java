85b64d6ea7fe25d9385a3d810db2baec039d3adf
silvanic <alexandre.dezier@orange.fr>
2017-06-13 14:19:58 +0200

    mise à jour main MonProjet class test

--- src/main/java/MonTest.java
+++ src/main/java/MonTest.java
@@ -3,6 +3,6 @@ import Utils.MyStringUtils;
 public class MonTest {
 	public static void main(String[] args) {
 		String debut = "Test is null or empty? : ";
-		System.out.println(debut + MyStringUtils.isNullorEmtpy("test"));
+		System.out.println(debut + MyStringUtils.isNullorEmtpy("Test"));
 	}
 }
